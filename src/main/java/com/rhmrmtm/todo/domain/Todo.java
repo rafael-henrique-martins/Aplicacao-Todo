9ef25c43a685212901d6c0cbce1d1e93be3d1cb9
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-09-07 15:33:55 -0300

    feat: Entity - anotacoes

--- src/main/java/com/rhmrmtm/todo/domain/Todo.java
+++ src/main/java/com/rhmrmtm/todo/domain/Todo.java
@@ -1,10 +1,34 @@
 package com.rhmrmtm.todo.domain;
 
-public class Todo {
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	}
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Todo implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dataParaFInalizar;
+	private Boolean finalizado = false;
 }
