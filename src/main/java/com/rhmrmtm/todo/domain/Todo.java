a16e2d5187db2e52376197ce115e72cba94dcddb
rafael-henrique-martins <rafael_henrique_martins@outlook.com>
2022-09-07 22:21:56 -0300

    feat: teste banco de dados

--- src/main/java/com/rhmrmtm/todo/domain/Todo.java
+++ src/main/java/com/rhmrmtm/todo/domain/Todo.java
@@ -8,18 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
-@Entity
+@Entity(name = "TODO")
 public class Todo implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -27,8 +16,89 @@ public class Todo implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+    
 	private String titulo;
 	private String descricao;
 	private LocalDateTime dataParaFInalizar;
 	private Boolean finalizado = false;
+	
+	public Todo() {}
+	
+	public Todo(Integer id, String titulo, String descricao, LocalDateTime dataParaFInalizar, Boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataParaFInalizar = dataParaFInalizar;
+		this.finalizado = finalizado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDataParaFInalizar() {
+		return dataParaFInalizar;
+	}
+
+	public void setDataParaFInalizar(LocalDateTime dataParaFInalizar) {
+		this.dataParaFInalizar = dataParaFInalizar;
+	}
+
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
